599c1280b50f320f60f76a1310681fac6b853ac0
jesus ochoa <jesus.ochoa.rabelo@gmail.com>
2021-12-30 23:07:45 -0700

    .

--- test/breaking_bad/client_test.rb
+++ test/breaking_bad/client_test.rb
@@ -12,21 +12,15 @@ module BreakingBad
       refute_nil Client.new("https://www.breakingbadapi.com/api/")
     end
 
-    # def test_client_must_raise_an_exception_when_path_missing
-    #   VCR.use_cassette("client_error") do
-    #     assert_raises BreakingBad::ClientError do
-    #       client.get("")
-    #     end
-    #   end
-    # end
-
-    def test_client_class_must_fetch_all_quotes
+    def test_client_class_must_fetch_quotes
       VCR.use_cassette("quotes") do
         assert_instance_of Array, client.get("quotes")
+        assert_instance_of Array, client.get("quotes?author=Jesse+Pinkman")
+        assert_instance_of Array, client.get("quotes/7")
       end
     end
 
-    def test_fetch_all_characters
+    def test_fetch_characters
       VCR.use_cassette("characters") do
         assert_instance_of Array, client.get("characters")
         assert_instance_of Hash, client.get("characters").first
@@ -34,7 +28,7 @@ module BreakingBad
       end
     end
 
-    def test_fetch_all_episodes
+    def test_fetch_episodes
       VCR.use_cassette("episodes") do
         assert_instance_of Array, client.get("episodes")
         assert_instance_of Hash, client.get("episodes").first
